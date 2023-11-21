cfa213ae4791537967864724b0cfe4cc51e617c7
ramss.solutions <RamsSolutions>
2023-11-21 10:34:24 +0200

    SpringBootServerInitializer

--- aqua/src/main/java/com/aqua/AquaApplication.java
+++ aqua/src/main/java/com/aqua/AquaApplication.java
@@ -2,10 +2,11 @@ package com.aqua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class AquaApplication {
+public class AquaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AquaApplication.class, args);
